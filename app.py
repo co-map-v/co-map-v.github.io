3e755f8ec99e9ca76aacf3f9567e7271da769505
nanhsun <nanhsunyuan@gmail.com>
2020-12-05 16:00:50 -0800

    Functionalized and removed redundant lines

--- app.py
+++ app.py
@@ -13,7 +13,6 @@
 # Authors: Aja Sutton, Andrew Teng, Jason Thomas, Nanhsuan Yuan
 # Autumn 2020
 
-
 import json
 import plotly.express as px
 import pandas as pd
@@ -23,20 +22,14 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 import urllib.request
 
-# Death Counts Map
+def death_counts_map(df_time,counties):
+    # Death Counts Map
 
-# Reads in a GeoJSON file and a demographics CSV file.
-# Generates a choropleth map of COVID-19 death counts by county for
-# the state of Massachusetts. Includes an interactive time slider
-# based on month of the year.
-
-with urllib.request.urlopen('https://raw.githubusercontent.com/co-map-v/co-map-v.github.io/main/data/ma_map.geojson') as response:
-    counties = json.load(response)
-
-df_time = pd.read_csv('https://raw.githubusercontent.com/co-map-v/co-map-v.github.io/main/data/covid_ma_positive_death_counts.csv',
-	dtype={'COUNTY': str})
-
-fig_death = px.choropleth(df_time, geojson=counties, locations="COUNTY",
+    # Reads in a GeoJSON file and a demographics CSV file.
+    # Generates a choropleth map of COVID-19 death counts by county for
+    # the state of Massachusetts. Includes an interactive time slider
+    # based on month of the year.
+    fig_death = px.choropleth(df_time, geojson=counties, locations="COUNTY",
 	featureidkey='properties.NAME',
 	color="death_counts",
 	title="Number of COVID-19 Deaths in Massachusetts (USA) <br> by County, January-March 2020",
@@ -46,25 +39,18 @@ fig_death = px.choropleth(df_time, geojson=counties, locations="COUNTY",
 	color_continuous_scale=px.colors.sequential.Blues,
 	range_color=[0,30],
 	animation_frame="condition_month")
-fig_death.update_geos(fitbounds="locations", visible=False)
-fig_death.update_layout(margin={"r":0,"t":50,"l":0,"b":0})
-#fig.show()
+    fig_death.update_geos(fitbounds="locations", visible=False)
+    fig_death.update_layout(margin={"r":0,"t":50,"l":0,"b":0})
+    return fig_death
 
+def case_count_map(df_time,counties):
+    # Case Count Map
 
-# Case Count Map
-
-# Reads in a GeoJSON file and a demographics CSV file.
-# Generates a choropleth map of positive COVID-19 case counts by
-# county for the state of Massachusetts. Includes an interactive
-# timeslider based on month of the year.
-
-with urllib.request.urlopen('https://raw.githubusercontent.com/co-map-v/co-map-v.github.io/main/data/ma_map.geojson') as response:
-    counties = json.load(response)
-
-df_time = pd.read_csv('https://raw.githubusercontent.com/co-map-v/co-map-v.github.io/main/data/covid_ma_positive_death_counts.csv',
-	dtype={'COUNTY': str})
-
-fig_case = px.choropleth(df_time, geojson=counties, locations="COUNTY",
+    # Reads in a GeoJSON file and a demographics CSV file.
+    # Generates a choropleth map of positive COVID-19 case counts by
+    # county for the state of Massachusetts. Includes an interactive
+    # timeslider based on month of the year.
+    fig_case = px.choropleth(df_time, geojson=counties, locations="COUNTY",
 	featureidkey='properties.NAME',
 	color="positive_counts",
 	title="Number of Positive COVID-19 Cases in Massachusetts (USA) <br> by County, January-March 2020",
@@ -74,24 +60,17 @@ fig_case = px.choropleth(df_time, geojson=counties, locations="COUNTY",
 	color_continuous_scale=px.colors.sequential.Blues,
 	range_color=[1,70],
 	animation_frame="condition_month")
-fig_case.update_geos(fitbounds="locations", visible=False)
-fig_case.update_layout(margin={"r":0,"t":50,"l":0,"b":0})
-#fig.show()
+    fig_case.update_geos(fitbounds="locations", visible=False)
+    fig_case.update_layout(margin={"r":0,"t":50,"l":0,"b":0})
+    return fig_case
 
+def population_map(df_time,counties):
+    # Population Map
 
-# Population Map
-
-# Reads in a GeoJSON file and a demographics CSV file.
-# Generates a choropleth map of population density by
-# county for the state of Massachusetts.
-
-with urllib.request.urlopen('https://raw.githubusercontent.com/co-map-v/co-map-v.github.io/main/data/ma_map.geojson') as response:
-    counties = json.load(response)
-
-df_time = pd.read_csv('https://raw.githubusercontent.com/co-map-v/co-map-v.github.io/main/data/covid_ma_positive_death_counts.csv',
-	dtype={'COUNTY': str})
-
-fig_pop = px.choropleth(df_time, geojson=counties, locations="COUNTY",
+    # Reads in a GeoJSON file and a demographics CSV file.
+    # Generates a choropleth map of population density by
+    # county for the state of Massachusetts.
+    fig_pop = px.choropleth(df_time, geojson=counties, locations="COUNTY",
 	featureidkey='properties.NAME',
 	color="population_2010",
 	title="Population by County, Massachusetts (USA), 2010 Census",
@@ -100,94 +79,95 @@ fig_pop = px.choropleth(df_time, geojson=counties, locations="COUNTY",
 	hover_name="COUNTY",
 	color_continuous_scale=px.colors.sequential.Blues,
 	range_color=[1,800000])
-fig_pop.update_geos(fitbounds="locations", visible=False)
-fig_pop.update_layout(margin={"r":0,"t":50,"l":0,"b":0})
-#fig.show()
+    fig_pop.update_geos(fitbounds="locations", visible=False)
+    fig_pop.update_layout(margin={"r":0,"t":50,"l":0,"b":0})
+    return fig_pop
 
+def deaths_histogram(df):
+    # Deaths Histogram
 
-# Population Histogram
-
-#read in the data
-df = pd.read_csv('https://raw.githubusercontent.com/co-map-v/co-map-v.github.io/main/data/covid_ma_positive_death_counts.csv')
-fig_pop_hist = px.histogram(df, x="COUNTY" , y="population_2010", 
-    title="Population by County, Massachusetts (USA), 2010 Census",
-    labels={'population_2010':'Population'},)
-
-fig_pop_hist.update_layout(
-    xaxis_title_text='County', # xaxis label
-    yaxis_title_text='Population', # yaxis label
-)
-
-# Cases Histogram
-
-#read in the data
-df = pd.read_csv('https://raw.githubusercontent.com/co-map-v/co-map-v.github.io/main/data/covid_ma_positive_death_counts.csv')
-fig_cases_hist = px.histogram(df, x="COUNTY" , y="positive_counts", 
-    title="Number of Positive COVID-19 Cases in Massachusetts (USA) <br> by County, January-March 2020",
-    labels={'population_2010':'Population',
-        'condition_month':'Month'},
-     animation_frame="condition_month")
-
-fig_cases_hist.update_layout(
-    xaxis_title_text='County', # xaxis label
-    yaxis_title_text='Total Cases', # yaxis label
-)
-
-# Deaths Histogram
-
-#read in the data
-df = pd.read_csv('https://raw.githubusercontent.com/co-map-v/co-map-v.github.io/main/data/covid_ma_positive_death_counts.csv')
-fig_death_hist = px.histogram(df, x="COUNTY" , y="death_counts", 
+    #read in the data
+    fig_death_hist = px.histogram(df, x="COUNTY" , y="death_counts", 
     title="Number of COVID-19 Deaths in Massachusetts (USA) <br> by County, January-March 2020",
     labels={'population_2010':'Population',
         'condition_month':'Month'},
      animation_frame="condition_month")
 
-fig_death_hist.update_layout(
-    xaxis_title_text='County', # xaxis label
-    yaxis_title_text='Total Deaths', # yaxis label
-)
+    fig_death_hist.update_layout(
+        xaxis_title_text='County', # xaxis label
+        yaxis_title_text='Total Deaths', # yaxis label
+    )
+    return fig_death_hist
 
+def case_histogram(df):
+    # Cases Histogram
 
-# Tab Content
+    #read in the data
+    fig_cases_hist = px.histogram(df, x="COUNTY" , y="positive_counts", 
+    title="Number of Positive COVID-19 Cases in Massachusetts (USA) <br> by County, January-March 2020",
+    labels={'population_2010':'Population',
+        'condition_month':'Month'},
+     animation_frame="condition_month")
 
-# Describes the content within the tabs. Injects HTML formatted graphs
-# into the tab window space.
+    fig_cases_hist.update_layout(
+        xaxis_title_text='County', # xaxis label
+        yaxis_title_text='Total Cases', # yaxis label
+    )
+    return fig_cases_hist
 
-tab1_content = dbc.Card(
-    dbc.CardBody([html.Div(dcc.Graph(figure=fig_case,))]),
-)
-tab2_content = dbc.Card(
-    dbc.CardBody([html.Div(dcc.Graph(figure=fig_death,))]),
-)
-tab3_content = dbc.Card(
-    dbc.CardBody([html.Div(dcc.Graph(figure=fig_pop,))]),
-)
-tab4_content = dbc.Card(
-    dbc.CardBody([html.Div(dcc.Graph(figure=fig_cases_hist,))]),
-)
-tab5_content = dbc.Card(
-    dbc.CardBody([html.Div(dcc.Graph(figure=fig_death_hist,))]),
-)
-tab6_content = dbc.Card(
-    dbc.CardBody([html.Div(dcc.Graph(figure=fig_pop_hist,))]),
-)
+def population_histogram(df):
+    # Population Histogram
 
-# Bootstrap Tabs
+    #read in the data
+    fig_pop_hist = px.histogram(df, x="COUNTY" , y="population_2010", 
+    title="Population by County, Massachusetts (USA), 2010 Census",
+    labels={'population_2010':'Population'},)
 
-# Declares the tabs using Bootstrap and Dash
+    fig_pop_hist.update_layout(
+        xaxis_title_text='County', # xaxis label
+        yaxis_title_text='Population', # yaxis label
+    )
+    return fig_pop_hist
 
-tabs = dbc.Tabs(
+def create_tabs(tabs_content:list):
+    # Bootstrap Tabs
+
+    # Declares the tabs using Bootstrap and Dash
+    tabs = dbc.Tabs(
     [
-        dbc.Tab(tab1_content, label="Cases (Map)"),
-        dbc.Tab(tab2_content, label="Deaths (Map)"),
-        dbc.Tab(tab3_content, label="Population (Map)"),
-        dbc.Tab(tab4_content, label="Cases (Chart)"),
-        dbc.Tab(tab5_content, label="Deaths (Chart)"),
-        dbc.Tab(tab6_content, label="Population (Chart)"),
-    ]
-)
+        dbc.Tab(tabs_content[0], label="Cases (Map)"),
+        dbc.Tab(tabs_content[1], label="Deaths (Map)"),
+        dbc.Tab(tabs_content[2], label="Population (Map)"),
+        dbc.Tab(tabs_content[3], label="Cases (Chart)"),
+        dbc.Tab(tabs_content[4], label="Deaths (Chart)"),
+        dbc.Tab(tabs_content[5], label="Population (Chart)"),
+    ])
+    return tabs
 
+with urllib.request.urlopen('https://raw.githubusercontent.com/co-map-v/co-map-v.github.io/main/data/ma_map.geojson') as response:
+    counties = json.load(response)
+df_time = pd.read_csv('https://raw.githubusercontent.com/co-map-v/co-map-v.github.io/main/data/covid_ma_positive_death_counts.csv',
+dtype={'COUNTY': str})
+
+fig_death = death_counts_map(df_time,counties)
+fig_case = case_count_map(df_time,counties)
+fig_pop = population_map(df_time,counties)
+
+fig_death_hist = deaths_histogram(df_time)
+fig_case_hist = case_histogram(df_time)
+fig_pop_hist = population_histogram(df_time)
+
+figs = [fig_case,fig_death,fig_pop,fig_case_hist,fig_death_hist,fig_pop_hist]
+tabs_content = []
+
+for i in range(len(figs)):
+    # Tab Content
+
+    # Describes the content within the tabs. Injects HTML formatted graphs
+    # into the tab window space.
+    tabs_content.append(dbc.Card(dbc.CardBody([html.Div(dcc.Graph(figure=figs[i],))]),))
+
+tabs = create_tabs(tabs_content)
 # Formats the layout of the Dash dashboard. Imports the Bootstrap and
 # Dash theme
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -196,4 +176,4 @@ server = app.server
 
 # Starts local server
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True)  
