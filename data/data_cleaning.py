4b7790d914cd7dcce10dcf6e91c9d34629dbe4b1
nanhsun <nanhsunyuan@gmail.com>
2020-12-09 22:53:12 -0800

    Added more unit tests and changed data_cleaning.py

--- data/data_cleaning.py
+++ data/data_cleaning.py
@@ -1,22 +1,23 @@
 '''Data was released Mar 29. Before summer uptick.
 SQL query below to extract and format data correctly
-SELECT DISTINCT l.zip, l.county, p.person_id, p.gender_source_value, p.birth_datetime, p.death_datetime, p.race_source_value, p.ethnicity_source_value, c.condition_start_datetime, c.condition_concept_id
+SELECT DISTINCT l.zip, l.county, p.person_id, p.gender_source_value,
+p.birth_datetime, p.death_datetime, p.race_source_value,
+p.ethnicity_source_value, c.condition_start_datetime,
+c.condition_concept_id
 FROM person p
 INNER JOIN condition_occurrence c ON c.person_id = p.person_id
 INNER JOIN location l on l.location_id = p.location_id
 WHERE condition_concept_id = '37311061' --Disease caused by 2019-nCoV
-
-
 ***********
 Must do all pep8 documentation still
 ***********
 '''
 
-import pandas as pd
+import json
 import os
-from os import path
-import os.path
 import pathlib
+import urllib.request
+import pandas as pd
 
 def read_patient_data(patient_data):
     '''
@@ -42,7 +43,7 @@ def read_pop_data(population_data):
     Reads in the data for population data (census data).
 
     Args:
-        population_data (str): the path to the census data. 
+        population_data (str): the path to the census data.
         data contains positive cases, gender, race/ethnicity, death, birth.
         Should be formatted like:
         *********************
@@ -55,22 +56,61 @@ def read_pop_data(population_data):
     if os.path.exists(file_to_read): #tests whether or not this csv file exists
         csv_as_df = pd.read_csv(file_to_read, encoding = 'utf-8')
     else:
-        raise NameError(f'{file_to_read} does not exist') #if file to read doesn't exist, raise error!
+        raise NameError(f'{file_to_read} does not exist')
+        # if file to read doesn't exist, raise error!
     return csv_as_df
 
 def county_cleaning(patient_dataset):
     '''
-    improve documentation
+    Clean up patient_dataset's county, condition_start_datetime, and death_datetime
+    columns.
+
+    Args:
+        patient_dataset (dataframe): pandas dataframe
+
+    Returns:
+        Two pandas dataframes
     '''
-    if 'county' not in patient_dataset:
-        raise NameError('No column named "county"')
-    if 'condition_start_datetime' not in patient_dataset:
-        raise NameError('No column named "condition_start_datetime"')
-    if 'death_datetime' not in patient_dataset:
-        raise NameError('No column named "death_datetime"')
+    columns = ['zip','county','person_id','gender_source_value','birth_datetime',
+                'death_datetime','race_source_value','ethnicity_source_value',
+                'condition_start_datetime','condition_concept_id']
+    incorrect_columns = []
+    county_names = []
+    incorrect_dtypes = []
+
+    for column in columns:
+        if column not in patient_dataset:
+            incorrect_columns.append(column)
+    if incorrect_columns != []:
+        raise NameError('No column named ' + str(incorrect_columns))
+
+    for column in patient_dataset.columns:
+        if column == 'zip':
+            if patient_dataset[column].dtype != 'float64':
+                incorrect_dtypes.append(column)
+        elif column == 'person_id' or column == 'condition_concept_id':
+            if patient_dataset[column].dtype != 'int64':
+                incorrect_dtypes.append(column)
+        else:
+            if patient_dataset[column].dtype != 'object':
+                incorrect_dtypes.append(column)
+    if incorrect_dtypes != []:
+        raise ValueError('The following columns have incorrect dtypes: ',
+                            incorrect_dtypes)
+
     data = patient_dataset
     #remove the word 'county' from the county column
     data['county'] = data['county'].str.split(' ').str[0]
+
+    with urllib.request.urlopen(
+        'https://raw.githubusercontent.com/co-map-v/co-map-v.github.io/main/data/ma_map.geojson')\
+        as response:
+        counties = json.load(response)
+    for i in range(len(counties['features'])):
+        county_names.append(counties['features'][i]['properties']['NAME'])
+    if set(pd.unique(data['county']).tolist()) != set(county_names):
+        raise ValueError('County name in column is different from GeoJSON county name')
+
     data['condition_month'] = pd.DatetimeIndex(data['condition_start_datetime']).month
     death = data [data['death_datetime'].notna()]
     return data,death
@@ -79,42 +119,86 @@ def county_cleaning(patient_dataset):
 #some persons have multiple entries (unique cases)
 def features_by_county(patient_data, death_data):
     '''
-    improve documentation
+    Groups cases by county
+
+    Args:
+        patient_data (dataframe): pandas dataframe
+        death_data (dataframe): pandas dataframe
+
+    Returns:
+        Two pandas dataframes
     '''
     data = patient_data
     death = death_data
     data_pos = data.groupby(['county',
                             'condition_month',
                             'gender_source_value',
-                            'race_source_value', 
+                            'race_source_value',
                             'ethnicity_source_value']).size().reset_index(name='positive_counts')
-    data_death = death.groupby(['county', 
+    data_death = death.groupby(['county',
                                 'condition_month',
                                 'gender_source_value',
-                                'race_source_value', 
+                                'race_source_value',
                                 'ethnicity_source_value']).size().reset_index(name='death_counts')
     return data_pos,data_death
 
-    
 def merge_data(data_pos, data_death, pop):
     '''
-    add documentation
+    Merge data_pos, data_death, and pop together as one dataframe
+
+    Args:
+        dadta_pos (dataframe): pandas dataframe
+        data_death (dataframe): pandas dataframe
+
+    Returns:
+        One pandas dataframe
     '''
-    data_flat = pd.merge(data_pos, data_death, on = ['county', 
-                                                        'condition_month', 
-                                                        'gender_source_value', 
+    incorrect_dtypes = []
+    columns = ['fips_code','county','population_2010']
+    incorrect_columns = []
+
+    for column in columns:
+        if column not in pop:
+            incorrect_columns.append(column)
+    if incorrect_columns != []:
+        raise NameError('No column named ' + str(incorrect_columns))
+
+    for column in pop.columns:
+        if column == 'fips_code' or column == 'population_2010':
+            if pop[column].dtype != 'int64':
+                incorrect_dtypes.append(column)
+        else:
+            if pop[column].dtype != 'object':
+                incorrect_dtypes.append(column)
+    if incorrect_dtypes != []:
+        raise ValueError('The following columns have incorrect dtypes: ',
+                            incorrect_dtypes)
+
+    data_flat = pd.merge(data_pos, data_death, on = ['county',
+                                                        'condition_month',
+                                                        'gender_source_value',
                                                         'race_source_value',
                                                         'ethnicity_source_value'], how="outer")
     data_flat_pop_fips = pd.merge(data_flat, pop, on = ['county'])
     data_flat_pop_fips.fillna(value=0, inplace=True) #filling in the NaN with 0
     return data_flat_pop_fips
 
-
 def write_file_for_viz(data_flat_pop_fips, path):
+    '''
+    Writes out pandas dataframe into csv file for visualization
+
+    Args:
+        data_flat_pop_fips (dataframe): pandas dataframe
+        path (string): save path for csv file
+
+    Returns:
+        None
+    '''
     nan_columns = []
     for column in data_flat_pop_fips.columns:
-        if False in data_flat_pop_fips[column].isna().tolist():
+        if True in data_flat_pop_fips[column].isna().tolist():
             nan_columns.append(column)
     if nan_columns != []:
         raise ValueError('Nan values in ' + str([column for column in nan_columns]))
-    data_flat_pop_fips.to_csv(path, sep = ',')
+
+    data_flat_pop_fips.to_csv(path, sep=',')
--- tests_alan.py
+++ tests_alan.py
@@ -1,62 +1,151 @@
 import unittest
+import os
+import numpy as np
 from data import data_cleaning
 import pandas as pd
-import numpy as np
-import unittest
-import os
-from os import path
-import os.path
 import pathlib
 
 
 class UnitTests(unittest.TestCase):
-    def test_column_names(self):  
-        df = pd.DataFrame({'county1': ['County1','County2'],'condition': [1,2],
-                            'deaths': [1,2]})      
-        with self.assertRaises(NameError):            
-            data_cleaning.county_cleaning(df)
-    
+    '''
+    Unit test class
+    '''
+    def test_column_names_for_covid_data(self):
+        '''
+        Test whether column names are correct for COVID data
+        '''
+        table = pd.DataFrame({'county1': ['County1','County2'],'condition': [1,2],
+                            'deaths': [1,2]})
+        with self.assertRaises(NameError):
+            data_cleaning.county_cleaning(table)
+
+    def test_column_names_for_pop_data(self):
+        '''
+        Test whether column names are correct for population data
+        '''
+        pop_table = pd.DataFrame({'county1': ['County1','County2'],'condition': [1,2],
+                            'deaths': [1,2]})
+        table1 = pd.DataFrame({'zip': [1,2],'county': [1,2],
+                            'person_id': [1,2],'gender_source_value': [1,2],
+                            'birth_datetime': [1,2], 'death_datetime': [1,2],
+                            'race_source_value': [1,2],
+                            'ethnicity_source_value': [1,2],
+                            'condition_start_datetime': [1,2],
+                            'condition_concept_id': [1,2]})
+        table2 = pd.DataFrame({'zip': [1,2],'county': [1,2],
+                            'person_id': [1,2],'gender_source_value': [1,2],
+                            'birth_datetime': [1,2], 'death_datetime': [1,2],
+                            'race_source_value': [1,2],
+                            'ethnicity_source_value': [1,2],
+                            'condition_start_datetime': [1,2],
+                            'condition_concept_id': [1,2]})
+        with self.assertRaises(NameError):
+            data_cleaning.merge_data(table1,table2,pop_table)
+
+    def test_column_type_in_county_cleaning(self):
+        '''
+        Test whether columns' types are correct for COVID data
+        '''
+        table = pd.DataFrame({'zip': [1,2],'county': [1,2],
+                            'person_id': [1,2],'gender_source_value': [1,2],
+                            'birth_datetime': [1,2], 'death_datetime': [1,2],
+                            'race_source_value': [1,2],
+                            'ethnicity_source_value': [1,2],
+                            'condition_start_datetime': [1,2],
+                            'condition_concept_id': [1,2]})
+        with self.assertRaises(ValueError):
+            data_cleaning.county_cleaning(table)
+
+    def test_column_type_in_merge_data(self):
+        '''
+        Test whether columns' types are correct for population data
+        '''
+        pop_table = pd.DataFrame({'fips_code':[1.5,1.6],'county':['King','King'],
+                                'population_2010':[1,0]})
+        table1 = pd.DataFrame({'zip': [1,2],'county': [1,2],
+                            'person_id': [1,2],'gender_source_value': [1,2],
+                            'birth_datetime': [1,2], 'death_datetime': [1,2],
+                            'race_source_value': [1,2],
+                            'ethnicity_source_value': [1,2],
+                            'condition_start_datetime': [1,2],
+                            'condition_concept_id': [1,2]})
+        table2 = pd.DataFrame({'zip': [1,2],'county': [1,2],
+                            'person_id': [1,2],'gender_source_value': [1,2],
+                            'birth_datetime': [1,2], 'death_datetime': [1,2],
+                            'race_source_value': [1,2],
+                            'ethnicity_source_value': [1,2],
+                            'condition_start_datetime': [1,2],
+                            'condition_concept_id': [1,2]})
+        with self.assertRaises(ValueError):
+            data_cleaning.merge_data(table1,table2,pop_table)
+
     def test_nan(self):
-        df = pd.DataFrame({'county1': [np.nan,'County2'],'condition': [1,2],
-                            'deaths': [1,2]})   
+        '''
+        Test there are nan values in columns
+        '''
+        table = pd.DataFrame({'county1': [np.nan,'County2'],'condition': [1,2],
+                            'deaths': [1,2]})
         path = ' '
         with self.assertRaises(ValueError):
-            data_cleaning.write_file_for_viz(df,path)
+            data_cleaning.write_file_for_viz(table,path)
 
-    def test_read_patient_data_output_type(self):  #CHANGE THIS TO BE THE TEST FOR THE TYPE OF WHAT WE WANT (PANDAS DF)
-        wd_of_script = pathlib.Path(__file__).parent.absolute() # get wd of this script being run where the data are
+    def test_county_names(self):
+        '''
+        Test whether county names are correctly named
+        '''
+        table = pd.DataFrame({'zip': [1,2],'county': ['KingCounty','King'],
+                            'person_id': [1,2],'gender_source_value': [1,2],
+                            'birth_datetime': [1,2], 'death_datetime': [1,2],
+                            'race_source_value': [1,2],
+                            'ethnicity_source_value': [1,2],
+                            'condition_start_datetime': [1,2],
+                            'condition_concept_id': [1,2]})
+        with self.assertRaises(ValueError):
+            data_cleaning.county_cleaning(table)
+
+    def test_read_patient_data_output_type(self):
+        # CHANGE THIS TO BE THE TEST FOR THE TYPE OF WHAT WE WANT (PANDAS DF)
+        # get wd of this script being run where the data are
+        wd_of_script = pathlib.Path(__file__).parent.absolute()
         patient_data_path = os.path.join(wd_of_script, 'data', 'data-1605136079581.csv')
-        df = data_cleaning.read_patient_data(patient_data_path)
-        self.assertTrue(isinstance(df, pd.DataFrame), 'data output is not in dataframe format')
+        dataframe = data_cleaning.read_patient_data(patient_data_path)
+        self.assertTrue(isinstance(dataframe, pd.DataFrame),
+                        'data output is not in dataframe format')
 
     def test_read_pop_data_output_type(self):
-        wd_of_script = pathlib.Path(__file__).parent.absolute() # get wd of this script being run where the data are
+        # get wd of this script being run where the data are
+        wd_of_script = pathlib.Path(__file__).parent.absolute()
         patient_data_path = os.path.join(wd_of_script, 'data', 'population2010.csv')
-        df = data_cleaning.read_pop_data(patient_data_path)
-        self.assertTrue(isinstance(df, pd.DataFrame), 'data output is not in dataframe format')
+        dataframe = data_cleaning.read_pop_data(patient_data_path)
+        self.assertTrue(isinstance(dataframe, pd.DataFrame),
+                        'data output is not in dataframe format')
 
     def feature_counts_greater_than_pop(self):
-        features_within_range = 'True' #set default
-        wd_of_script = pathlib.Path(__file__).parent.absolute() # get wd of this script being run where the data are
-        filepath_read = os.path.join(wd_of_script, 'data', 'covid_ma_positive_death_counts.csv') #ensure this is the name of your output file fed into viz
-        df = pd.read_csv(filepath_read)
-        df['death_pop_ratio'] = df['death_counts']/df['population_2010']
-        df['cases_pop_ratio'] = df['positive_counts']/df['population_2010']
-        max_death_pop_ratio = df['death_pop_ratio'].max()
-        max_cases_pop_ratio = df['cases_pop_ratio'].max()
+        features_within_range = 'True' # set default
+        # get wd of this script being run where the data are
+        wd_of_script = pathlib.Path(__file__).parent.absolute()
+        #ensure this is the name of your output file fed into viz
+        filepath_read = os.path.join(wd_of_script, 'data', 'covid_ma_positive_death_counts.csv')
+        dataframe = pd.read_csv(filepath_read)
+        dataframe['death_pop_ratio'] = dataframe['death_counts']/dataframe['population_2010']
+        dataframe['cases_pop_ratio'] = dataframe['positive_counts']/dataframe['population_2010']
+        max_death_pop_ratio = dataframe['death_pop_ratio'].max()
+        max_cases_pop_ratio = dataframe['cases_pop_ratio'].max()
         if max_death_pop_ratio > 1 | max_cases_pop_ratio > 1:
             features_within_range = 'False'
-        self.assertTrue(features_within_range, 'True')#, 'feature counts (deaths or cases) exceed population')
+        self.assertTrue(features_within_range, 'True')
+        #, 'feature counts (deaths or cases) exceed population')
 
-    def test_write_file_for_viz(self): #must adjust to be the data_fips data but as a toy dataset
-        wd_of_script = pathlib.Path(__file__).parent.absolute() # get wd of this script being run where the data are
+    def test_write_file_for_viz(self): # must adjust to be the data_fips data but as a toy dataset
+        # get wd of this script being run where the data are
+        wd_of_script = pathlib.Path(__file__).parent.absolute()
         filepath_read = os.path.join(wd_of_script, 'data', 'covid_ma_positive_death_counts.csv')
-        df = pd.read_csv(filepath_read)
-        filename_to_write = 'test_cleaned_dataset.csv' #dummy output to then be deleted later
-        data_cleaning.write_file_for_viz(df, filename_to_write) #writes the csv file
-        self.assertTrue(os.path.exists(filename_to_write)) #does the csv we attempted to write exist?
-        if os.path.exists(filename_to_write): #tests whether or not this csv file exists
-            os.remove(filename_to_write) #removes the file if it exists
+        dataframe = pd.read_csv(filepath_read)
+        filename_to_write = 'test_cleaned_dataset.csv' # dummy output to then be deleted later
+        data_cleaning.write_file_for_viz(dataframe, filename_to_write) # writes the csv file
+        self.assertTrue(os.path.exists(filename_to_write)) # does the csv we write exist?
+        if os.path.exists(filename_to_write): # tests whether or not this csv file exists
+            os.remove(filename_to_write) # removes the file if it exists
         else:
             pass
 
