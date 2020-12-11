6229bd1b2ea39ed3d33c378572a87d59b6ab5764
Aja M. Sutton <amsutton@uw.edu>
2020-12-11 12:07:33 -0800

    attempted to fix data import errors

--- tests/tests_aja.py
+++ tests/tests_aja.py
@@ -1,13 +1,13 @@
 import unittest
 import os
 import json
-import pathlib
+import urllib.request
+import pandas as pd 
 import plotly.express as px
 
 class UnitTests(unittest.TestCase):
 
     def smoke_test1(self):
-    #def smoke_test1(df_time,counties):
         """Smoke Test: Death Counts Map
 
         Check if Plotly Express map code returns a choropleth map
@@ -18,35 +18,34 @@ class UnitTests(unittest.TestCase):
 
         Args:
             counties (GeoJSON)
-            df_time (pandas dataframe): 'COUNTY' variable set to string.
+            df_time (pandas dataframe): 'COUNTY' variable is a string.
 
         Returns:
-            fig_death (Ploty Express figure)
+            fig_death (Ploty figure)
         """
-        #Smoke tests data loaded as global variables to avoid redownloading
-        with open('./data/ma_map.geojson', 'r') as response:
-            counties = json.load(response)
+        with urllib.request.urlopen('https://raw.githubusercontent.com/co-map-v/co-map-v.github.io/main/data/ma_map.geojson') as response:
+    		counties_1 = json.load(response)
 
-        wd_of_script = pathlib.Path(__file__).parent.absolute()
-        df_time = os.path.join(wd_of_script, '/', 'smoketest_data.csv')
+        df_time = pd.read_csv('https://github.com/co-map-v/co-map-v.github.io/blob/main/tests/smoketest_data.csv',
+			dtype={'COUNTY': str})
 
         death_range_max = max(df_time.death_counts)
         fig_death = px.choropleth(df_time, geojson=counties, locations="COUNTY",
-        featureidkey='properties.NAME',
-        color="death_counts",
-        title="Number of COVID-19 Deaths in Massachusetts (USA) <br> by County, January-March 2020",
-        labels={'death_counts':'Number of Deaths',
-            'condition_month':'Month'},
-        hover_name="COUNTY",
-        color_continuous_scale=px.colors.sequential.Reds,
-        range_color=[0,death_range_max],
-        animation_frame="condition_month")
+	        featureidkey='properties.NAME',
+	        color="death_counts",
+	        title="Number of COVID-19 Deaths in Massachusetts (USA)",
+	        	"<br> by County, January-March 2020",
+	        labels={'death_counts':'Number of Deaths',
+	            'condition_month':'Month'},
+	        hover_name="COUNTY",
+	        color_continuous_scale=px.colors.sequential.Reds,
+	        range_color=[0,death_range_max],
+	        animation_frame="condition_month")
         fig_death.update_geos(fitbounds="locations", visible=False)
         fig_death.update_layout(margin={"r":0,"t":50,"l":0,"b":0})
         return fig_death
 
     def smoke_test2(self):
-    #def smoke_test2(df_time,counties):
         """Smoke Test: Case Count Map
 
         Check if Plotly Express map code returns a choropleth map
@@ -60,26 +59,26 @@ class UnitTests(unittest.TestCase):
             counties
 
         Returns:
-            fig_case (Ploty Express figure)
+            fig_case (Ploty figure)
         """
-        #Smoke tests data loaded as global variables to avoid redownloading
-        with open('./data/ma_map.geojson', 'r') as response:
-            counties = json.load(response)
+        with urllib.request.urlopen('https://raw.githubusercontent.com/co-map-v/co-map-v.github.io/main/data/ma_map.geojson') as response:
+    		counties_1 = json.load(response)
 
-        wd_of_script = pathlib.Path(__file__).parent.absolute()
-        df_time = os.path.join(wd_of_script, '/', 'smoketest_data.csv')
+        df_time = pd.read_csv('https://github.com/co-map-v/co-map-v.github.io/blob/main/tests/smoketest_data.csv',
+			dtype={'COUNTY': str})
 
         pos_range_max = max(df_time.positive_counts)
         fig_case = px.choropleth(df_time, geojson=counties, locations="COUNTY",
-        featureidkey='properties.NAME',
-        color="positive_counts",
-        title="Number of Positive COVID-19 Cases in Massachusetts (USA) <br> by County, January-March 2020",
-        labels={'positive_counts':'Number of Cases',
-            'condition_month':'Month'},
-        hover_name="COUNTY",
-        color_continuous_scale=px.colors.sequential.Blues,
-        range_color=[1,pos_range_max],
-        animation_frame="condition_month")
+	        featureidkey='properties.NAME',
+	        color="positive_counts",
+	        title="Number of Positive COVID-19 Cases in Massachusetts (USA)",
+	        	"<br> by County, January-March 2020",
+	        labels={'positive_counts':'Number of Cases',
+	            'condition_month':'Month'},
+	        hover_name="COUNTY",
+	        color_continuous_scale=px.colors.sequential.Blues,
+	        range_color=[1,pos_range_max],
+	        animation_frame="condition_month")
         fig_case.update_geos(fitbounds="locations", visible=False)
         fig_case.update_layout(margin={"r":0,"t":50,"l":0,"b":0})
         return fig_case
